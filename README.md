a8d8f8a335b6f0f8b353498f1cba22edd037889f
Long Nguyen <2006.long.nguyen@gmail.com>
2018-06-27 12:14:17 +1000

    Add bot aliases

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # slack_luncherbot
 
-A Slack bot that allows your team to vote for lunch options!
+A Slack bot that allows your team to vote for lunch options! Built on [slack-ruby-bot](https://github.com/slack-ruby/slack-ruby-bot)
+
+## to add:
+- voting system
+- number of voters/users online
+- register 1 vote per user
+- attach menu link to the choice
--- luncherbot.rb
+++ luncherbot.rb
@@ -3,10 +3,15 @@ require 'giphy'
 
 class LuncherBot < SlackRubyBot::Bot
     command 'vote' do |client, data, match|
-        client.say(text: "<@#{data.user}> initiated a vote! Please choose:", channel: data.channel)
-        client.say(text: '1 - pizza', channel: data.channel)
-        client.say(text: '2 - grilld', channel: data.channel)
-        client.say(text: '3 - nandos', channel: data.channel)
+        client.say(text: "<@#{data.user}> initiated a vote! Please choose:\n1 - pizza\n2 - grilld\n3 - nandos", channel: data.channel)
+        op1, op2, op3 = 0
+    end
+    command '1' do |client, data, match|
+        op1 += 1
+        client.say(text: "#{op1} vote for pizza", channel: data.channel)
     end
 end
+SlackRubyBot.configure do |config|
+    config.aliases = ['lunch', 'food']
+  end
 LuncherBot.run
