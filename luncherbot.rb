95807fcc0d181797dbca5772be12bee7651c550e
Long Nguyen <2006.long.nguyen@gmail.com>
2018-06-27 12:18:29 +1000

    Add 'help' command

--- luncherbot.rb
+++ luncherbot.rb
@@ -2,6 +2,17 @@ require 'slack-ruby-bot'
 require 'giphy'
 
 class LuncherBot < SlackRubyBot::Bot
+    help do
+        title 'Luncherbot'
+        desc 'Vote for your lunch option!'
+    
+        command 'vote' do
+          desc 'Initiates a vote'
+          long_desc "Accurate 10 Day Weather Forecasts for thousands of places around the World.\n" \
+            'Bot provides detailed Weather Forecasts over a 10 day period updated four times a day.'
+        end
+    end
+
     command 'vote' do |client, data, match|
         client.say(text: "<@#{data.user}> initiated a vote! Please choose:\n1 - pizza\n2 - grilld\n3 - nandos", channel: data.channel)
         op1, op2, op3 = 0
