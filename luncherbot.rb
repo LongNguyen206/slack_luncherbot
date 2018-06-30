ee6214fd86491742dcba25906e8a5a8fdb4e470e
Long Nguyen <2006.long.nguyen@gmail.com>
2018-06-30 19:01:31 +1000

    Add votes counter and end-of-voting trigger. First working version

--- luncherbot.rb
+++ luncherbot.rb
@@ -1,6 +1,5 @@
 require 'slack-ruby-bot'
 require 'giphy'
-require 'httparty'
 
 # SlackRubyBot::Client.logger.level = Logger::WARN
 
@@ -10,26 +9,74 @@ class LuncherBot < SlackRubyBot::Bot
         title 'Luncherbot'
         desc 'Vote for your lunch option!'
     
-        command 'vote' do
+        command 'vote <number_of_voters>' do
           desc 'Initiates a vote'
-          long_desc "Accurate 10 Day Weather Forecasts for thousands of places around the World.\n" \
-            'Bot provides detailed Weather Forecasts over a 10 day period updated four times a day.'
+          long_desc "Initiates a vote from a predetermined list of restaurants.\n" \
+            'The bot will keep registering votes equal to the number of voters.'
         end
     end
-
-    match(/^vote\s(?<input>\w*)$/) do |client, data, match|
+    @vote_initiated = false
+    # use hashes for options and vote count
+    match(/^lunch vote\s(?<input>\w*)$/) do |client, data, match|
         if match['input'] =~ /^(0+)$/
             client.say(text: "There is noone here to vote :cry:\n", channel: data.channel, gif: "alone")
+            @vote_initiated = false
         elsif match['input'] =~ /^(\d+)$/ 
-            vote_num = match['input'].to_i
-            client.say(text: "<@#{data.user}> initiated a vote! There are #{vote_num} people voting. Please choose:\n1 - pizza\n2 - grilld\n3 - nandos", channel: data.channel)
-            op1, op2, op3 = 0
-
+            @vote_initiated = true
+            @vote_num = match['input'].to_i
+            client.say(text: "<@#{data.user}> initiated a vote! There are #{@vote_num} people voting. Please choose:\n1 - pizza\n2 - Grill'd\n3 - Nandos", channel: data.channel)
+            @op1 = 0
+            @op2 = 0
+            @op3 = 0
+            @votes = 0
+            # @options = { pizza: 0, Grilld: 0, Nandos: 0 }
         else
-            client.say(text: "Cmooooon! '#{match['input']}' is not a number!!! :expressionless:\n", channel: data.channel, gif: "dumb")
+            client.say(text: "Cmooooon! '#{match['input']}' is not a number!!! :expressionless:\n", channel: data.channel, gif: "idiot")
+            @vote_initiated = false
         end     
     end
 
+    
+        match(/^(?<choice>\w)$/) do |client, data, match|
+            # if @votes <= @vote_num
+                if @vote_initiated == true
+                    if @votes < @vote_num
+                        if match['choice'].to_i == 1
+                            @op1 += 1
+                            @votes += 1
+                            if @op1 == 1
+                                client.say(text: "#{@op1} vote for pizza!\n", channel: data.channel)
+                            else
+                                client.say(text: "#{@op1} votes for pizza!\n", channel: data.channel)
+                            end
+                        elsif match['choice'].to_i == 2
+                            @op2 += 1
+                            @votes += 1
+                            if @op2 == 1
+                                client.say(text: "#{@op2} vote for Grill'd!\n", channel: data.channel)
+                            else
+                                client.say(text: "#{@op2} votes for Grill'd!\n", channel: data.channel)
+                            end
+                        elsif match['choice'].to_i == 3
+                            @op3 += 1
+                            @votes += 1
+                            if @op3 == 1
+                                client.say(text: "#{@op3} vote for Nandos!\n", channel: data.channel)
+                            else
+                                client.say(text: "#{@op3} votes for Nandos!\n", channel: data.channel)
+                            end
+                        else
+                            client.say(text: "No such option!\n", channel: data.channel, gif: 'idiot')
+                        end
+                    else
+                        client.say(text: "All votes counted!\n", channel: data.channel)
+                        @vote_initiated = false
+                    end
+                else
+                    client.say(text: "Please initiate a vote!\n", channel: data.channel)
+                end
+            # end
+        end
     # class << self
     #     def parse_input(_match)
     #       limit = _match[:expression] if limit.to_i.to_s == limit
