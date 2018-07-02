49202b9a66528f300b82f0e8ae3a1733677abf45
Long Nguyen <2006.long.nguyen@gmail.com>
2018-07-02 14:14:46 +1000

    Allow voting with option names

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ A Slack bot that allows your team to vote for lunch options! Built on [slack-rub
 
 ## to add:
 - register 1 vote per user!
-- randomize winners if > 1
 - output results as attachment
-- randomized message only if several winners
+- 'randomized' message only if several winners
 - attach menu link to the choice
 - allow 'vote <list of option>' cmd, where user can list names of things he wants
 - put in methods, classes and modules! (Adult code)
+- change string output depending on condition (miltiples)
--- luncherbot.rb
+++ luncherbot.rb
@@ -17,8 +17,8 @@ class LuncherBot < SlackRubyBot::Bot
     end
 
     @vote_initiated = false
-    # use hashes for options and vote count
-    match(/^vote\s(?<input>\w*)$/) do |client, data, match|
+
+    match(/^(v|V)ote\s(?<input>\w*)$/) do |client, data, match|
         if match['input'] =~ /^(0+)$/
             client.say(text: "There is noone here to vote :cry:\n", channel: data.channel, gif: "alone")
             @vote_initiated = false
@@ -28,19 +28,16 @@ class LuncherBot < SlackRubyBot::Bot
             @vote_num = match['input'].to_i
             client.say(text: "<@#{data.user}> initiated a vote! There are #{@vote_num} people voting. Please choose:\n1 - #{@options[0][:name]}\n2 - #{@options[1][:name]}\n3 - #{@options[2][:name]}", channel: data.channel)
             @votes_current = 0
-            # @op1 = 0
-            # @op2 = 0
-            # @op3 = 0
         else
             client.say(text: "Cmooooon! '#{match['input']}' is not a number!!! :expressionless:\n", channel: data.channel, gif: "idiot")
             @vote_initiated = false
         end     
     end
 
-    match(/^(?<choice>\w)$/) do |client, data, match|
+    match(/^(?<choice>\w*)$/) do |client, data, match|
         if @vote_initiated == true
             if @votes_current < @vote_num
-                if match['choice'].to_i == 1
+                if match['choice'] =~ /1|#{@options[0][:name]}/
                     @votes_current += 1
                     @options[0][:votes] += 1
                     if @options[0][:votes] == 1
@@ -48,7 +45,7 @@ class LuncherBot < SlackRubyBot::Bot
                     else
                         client.say(text: "#{@options[0][:votes]} votes for #{@options[0][:name]}!\n", channel: data.channel)
                     end
-                elsif match['choice'].to_i == 2
+                elsif match['choice'] =~ /2|#{@options[1][:name]}/
                     @votes_current += 1
                     @options[1][:votes] += 1
                     if @options[1][:votes] == 1
@@ -56,14 +53,10 @@ class LuncherBot < SlackRubyBot::Bot
                     else
                         client.say(text: "#{@options[1][:votes]} votes for #{@options[1][:name]}!\n", channel: data.channel)
                     end
-                elsif match['choice'].to_i == 3
+                elsif match['choice'] =~ /3|#{@options[2][:name]}/
                     @votes_current += 1
                     @options[2][:votes] += 1
-                    if @options[2][:votes] == 1
-                        client.say(text: "#{@options[2][:votes]} vote for #{@options[2][:name]}!\n", channel: data.channel)
-                    else
-                        client.say(text: "#{@options[2][:votes]} votes for #{@options[2][:name]}!\n", channel: data.channel)
-                    end
+                    client.say(text: "#{@options[2][:votes]} #{@options[2][:votes] == 1 ? "vote" : "votes"} for #{@options[2][:name]}!\n", channel: data.channel)
                 else
                     client.say(text: "No such option!\n", channel: data.channel, gif: 'idiot')
                 end
@@ -84,11 +77,11 @@ class LuncherBot < SlackRubyBot::Bot
             end
 
             if @votes_current == @vote_num
-                client.say(text: "Voting is over\n", channel: data.channel)
+                client.say(text: "*Voting is over*\n", channel: data.channel)
                 @options.each do |hash|
-                    client.say(text: "Option #{hash[:name]} got #{hash[:votes]} votes\n", channel: data.channel)
+                    client.say(text: "Option *#{hash[:name]}* got #{hash[:votes]} #{hash[:votes] == 1 ? "vote" : "votes"}\n", channel: data.channel)
                 end
-                client.say(text: "The winner is #{winner}\n (randomized in case of equal votes)", channel: data.channel)
+                client.say(text: "The winner is #{winner} (randomized in case of equal votes)", channel: data.channel)
             end
         else
             client.say(text: "Please initiate a vote!\n", channel: data.channel)
